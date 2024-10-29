8f66bb9eb9ee0fdae582b05d1a726e8ac28f722f
Juwon Jeong <2jw0718@gmail.com>
2024-10-29 11:42:59 +0900

    카메라 기능 추가

--- App.tsx
+++ App.tsx
@@ -1,11 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { useRef, useState } from "react";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef<CameraView>(null);
+  const [photo, setPhoto] = useState<string | null>(null);
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  async function takePicture() {
+    if (cameraRef.current) {
+      const picture = await cameraRef.current.takePictureAsync();
+      console.log(picture);
+      picture && setPhoto(picture.uri);
+    }
+  }
+
+  if (photo) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: photo }}
+          style={styles.photo}
+          resizeMode="cover"
+        />
+        <Text style={styles.message}>Photo taken!</Text>
+        <Button onPress={() => setPhoto(null)} title="Take another photo" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView
+        style={styles.camera}
+        facing={facing}
+        ref={cameraRef}
+        videoQuality="480p"
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr"],
+        }}
+        onBarcodeScanned={(barcode) => {
+          console.log("Barcode scanned", barcode);
+        }}
+        flash="on"
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
@@ -13,8 +89,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    width: 400,
+    height: 400,
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  photo: {
+    width: 400,
+    height: 400,
+    alignSelf: "center",
+    borderRadius: 200,
   },
 });
